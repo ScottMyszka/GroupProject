106af6390b8ec3786a241e9229e82086b0a75d82
thiru <thirupathamma07@gmail.com>
2022-04-09 20:13:47 +1000

    basic view of page

---
+++ js/validate.js
@@ -0,0 +1,8 @@
+
+     let validateTheForm=() => {
+         let firstname=document.getElementById('firstname').value;
+         let lastname=document.getElementById('lastname').value;
+         console.log(firstname +' '+lastname);
+         console.log(hi);
+         document.getElementById('firstname')='hello i am';
+     }
