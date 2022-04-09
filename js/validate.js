f84e01a6511be17df6eb0bffdee2905754558728
thiru <thirupathamma07@gmail.com>
2022-04-09 20:22:32 +1000

    Basic view of spaceship booking

--- js/validate.js
+++ js/validate.js
@@ -3,6 +3,5 @@
          let firstname=document.getElementById('firstname').value;
          let lastname=document.getElementById('lastname').value;
          console.log(firstname +' '+lastname);
-         console.log(hi);
-         document.getElementById('firstname')='hello i am';
+         
      }
