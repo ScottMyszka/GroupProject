fe17c1e1bed4d13ca0eec7584a72edd5e9178a58
penrique <patricke258@gmail.com>
2022-04-13 03:27:32 +1000

    -minor fixes

--- js/validation.js
+++ js/validation.js
@@ -45,11 +45,11 @@ function validateFirstName() {
   messages = [];
   var regex = /^[A-Za-z\s]+$/;
   if (firstname.value === "" || firstname.value == null) {
-    messages.push("First name is required");
+    messages.push("First name is required.");
   } else if (!regex.test(firstname.value)) {
-    messages.push("First name must not contain numbers or special characters");
+    messages.push("First name must not contain numbers or special characters.");
   } else if (firstname.value.length <= 2) {
-    messages.push("First name should be more than 2 characters");
+    messages.push("First name should be more than 2 characters.");
   }
   if (messages.length > 0) {
     firstnameError.innerText = messages.join(", ");
@@ -66,11 +66,11 @@ function validateLastName() {
   var regex = /^[A-Za-z\s]+$/;
 
   if (lastname.value === "" || lastname.value == null) {
-    messages.push("Last name is required");
+    messages.push("Last name is required.");
   } else if (!regex.test(lastname.value)) {
-    messages.push("Last name must not contain numbers or special characters");
+    messages.push("Last name must not contain numbers or special characters.");
   } else if (lastname.value.length <= 2) {
-    messages.push("Last name should be more than 2 characters");
+    messages.push("Last name should be more than 2 characters.");
   }
 
   if (messages.length > 0) {
@@ -87,9 +87,9 @@ function validateEmail() {
   let messages = [];
   let mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/ ;
   if (email.value === "" || email.value == null) {
-    messages.push("Email is required");
+    messages.push("Email is required.");
   } else if (!mailRegex.test(email.value)) {
-    messages.push("Please enter a proper email");
+    messages.push("Please enter a proper email.");
   }
   if (messages.length > 0) {
     emailError.innerText = messages.join(", ");
@@ -105,11 +105,11 @@ function validatePhone() {
   let messages = [];
   let phoneRegex = "^[0-9]*$";
   if (phone.value === "" || phone.value == null) {
-    messages.push("Phone number is required");
+    messages.push("Phone number is required.");
   } else if (!phone.value.match(phoneRegex)) {
-    messages.push("Phone number only allows numerical chracters");
+    messages.push("Phone number only allows numerical chracters.");
   } else if (!(phone.value.length === 10)) {
-    messages.push("Phone number must have 10 numbers");
+    messages.push("Phone number must have 10 numbers.");
   }
   if (messages.length > 0) {
     phoneError.innerText = messages.join(", ");
@@ -127,24 +127,24 @@ function validateTravelDates() {
   let departDate1;
   let returnDate1;
   if (departDate.value === "" || departDate.value === null) {
-    messages.push("Please select your depart travel date");
+    messages.push("Please select your depart travel date.");
   } else {
     departDate1 = new Date(departDate.value);
 
     if (!(departDate1 >= today)) {
-      messages.push("Depart date must be today or greater than today");
+      messages.push("Depart date cannot be before today.");
     }
   }
   if (roundtrip.checked == true) {
     if (returnDate.value === "" || returnDate.value === null) {
-      messages.push("Please select your arrival travel date");
+      messages.push("Please select your arrival travel date.");
     } else {
       returnDate1 = new Date(returnDate.value);
       if (!(returnDate1 >= today)) {
-        messages.push("Return date must be today or greater than today");
+        messages.push("Return date cannot be before today.");
       }
       if (returnDate1 <= departDate1) {
-        messages.push("Return date must be greater than depart date");
+        messages.push("Return date cannot be before Depart date.");
       }
     }
   }
@@ -163,11 +163,11 @@ function validateCreditcard() {
 
   let regex = "^[0-9]*$";
   if (creditcard.value === "" || creditcard.value == null) {
-    messages.push("Credit card field is required");
+    messages.push("Credit card field is required.");
   } else if (!creditcard.value.match(regex)) {
-    messages.push("Credit card field can only be numbers");
+    messages.push("Credit card field can only be numbers.");
   } else if (!(creditcard.value.length === 10)) {
-    messages.push("credit card field number has to be 10 values");
+    messages.push("credit card field number has to be 10 values.");
   }
   if (messages.length > 0) {
     creditcardError.innerText = messages.join(", ");
@@ -188,9 +188,9 @@ function validateExpiryDate() {
 
   var result = [];
   if (expire.value === "" || expire.value === null) {
-    messages.push("Please enter expiry date of your card");
+    messages.push("Please enter expiry date of your card.");
   } else if (!reg.test(expire.value)) {
-    messages.push("Please enter in mm/yy format,only numbers allowed");
+    messages.push("Please enter in the month and the year in the mm/yy format as numbers.");
   } else {
     result = expire.value.toString().split("/");
   }
@@ -200,7 +200,7 @@ function validateExpiryDate() {
 
   if (year > exYear || (year === exYear && month >= exMonth)) {
     messages.push(
-      "The expiry date is before today's date. Please select a valid expiry date"
+      "The expiry date is before today's date. Please select a valid expiry date."
     );
   }
   if (messages.length > 0) {
@@ -216,7 +216,7 @@ function validateExpiryDate() {
 function validateFromField() {
   let messages = [];
   if (from.value === "" || from.value == null) {
-    messages.push("Please select any city from From feild");
+    messages.push("Please select any city from From field.");
   }
   if (messages.length > 0) {
     fromError.innerText = messages.join(", ");
@@ -231,7 +231,7 @@ function validateFromField() {
 function validateToField() {
   let messages = [];
   if (to.value === "" || to.value == null) {
-    messages.push("Please select any city from To feild");
+    messages.push("Please select any city from To field.");
   }
   if (messages.length > 0) {
     toError.innerText = messages.join(", ");
@@ -247,11 +247,11 @@ function validateCSC() {
   let messages = [];
   let regex = "^[0-9]*$";
   if (code.value === "" || code.value == null) {
-    messages.push("CSC field is required");
+    messages.push("CSC field is required.");
   } else if (!code.value.match(regex)) {
-    messages.push("CSC can only be numbers");
+    messages.push("CSC can only be numbers.");
   } else if (!(code.value.length === 3)) {
-    messages.push("CSC number has to be 3 values");
+    messages.push("CSC number has to be 3 values.");
   }
   if (messages.length > 0) {
     cscError.innerText = messages.join(", ");
@@ -277,7 +277,7 @@ const headCount = document.getElementById("headcount");
 const passenger2 = document.getElementById("passenger2")
 const passenger3 = document.getElementById("passenger3")
 //Number of passengers
-function test() {
+function numberOfPeople() {
   if (headCount.value == 1) {
     passenger2.style.display ="none"
     passenger3.style.display ="none";
@@ -290,7 +290,7 @@ function test() {
   }
 }
 
-//set default data to all feilds on the form
+//set default data to all fields on the form
 function setDefaultData() {
   firstname.innerText = "";
   lastname.innerText = "";
@@ -306,7 +306,7 @@ function setDefaultData() {
   departDate.innerText = "";
   returnDate.innerText = "";
 }
-//set default error messages to all feilds on the form
+//set default error messages to all fields on the form
 function setDefaultErrorMessages() {
   firstnameError.innerText = "";
   lastnameError.innerText = "";
