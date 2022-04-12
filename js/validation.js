24ee9a1565de6ef4712b202825302c6a387fc903
thiru <thirupathamma07@gmail.com>
2022-04-12 17:33:26 +1000

    date validations

--- js/validation.js
+++ js/validation.js
@@ -3,8 +3,8 @@ const firstname = document.getElementById("firstname");
 const lastname = document.getElementById("lastname");
 const form = document.getElementById("form");
 const errorElement = document.getElementById("error");
-const from = document.getElementById("from");
-const to = document.getElementById("to");
+const from = document.getElementById("departureCity");
+const to = document.getElementById("destinationCity");
 const oneway = document.getElementById("oneway");
 const roundtrip = document.getElementById("roundtrip");
 const trip = document.getElementsByClassName("trip");
@@ -12,19 +12,23 @@ const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const departDate = document.getElementById("departDate");
 const returnDate = document.getElementById("returnDate");
-const destinationCity = document.getElementById('destinationCity')
-const creditcard = document.getElementById('card')
-const expire = document.getElementById('expire')
-const code = document.getElementById('code')
+const creditcard = document.getElementById("card");
+const expire = document.getElementById("expire");
+const code = document.getElementById("code");
 let messages = [];
 
 form.addEventListener("submit", (e) => {
   messages = [];
-  validateFirstName();
-  validateLastName();
-  validateEmail();
-  validatePhone();
-  validateCreditcard();
+  // validateFromField();
+  // validateToField();
+  //validateFirstName();
+  // validateLastName();
+  // validateEmail();
+  // validatePhone();
+  // validateCreditcard();
+  // validateCSC();
+  validateTravelDates();
+  //validateExpiryDate();
 
   if (messages.length > 0) {
     e.preventDefault();
@@ -58,13 +62,15 @@ function validateLastName() {
 
 //Validation rule on email
 function validateEmail() {
-  let mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+  let mailRegex =
+    /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
   if (email.value === "" || email.value == null) {
     messages.push("email is required");
   } else if (!mailRegex.test(email.value)) {
     messages.push("Please enter proper email");
   }
 }
+
 //Validation rule on phone number
 function validatePhone() {
   let phoneRegex = "^[0-9]*$";
@@ -79,52 +85,123 @@ function validatePhone() {
 
 //Validation rule on booking dates
 function validateTravelDates() {
-  let today = new date();
-  if (departDate.value < today) {
-    messages.push("Depart date must be today or greater than today");
-  }
-  if (roundtrip.checked == true) {
-    if (returnDate < departDate) {
-      messages.push("Return date must be greater than depart date");
+  let today = new Date();
+  let departDate1;
+  let returnDate1;
+  if (departDate.value === "" || departDate.value === null) {
+    messages.push("Please select your depart travel date");
+  } else {
+    departDate1 = new Date(departDate.value);
+   
+    if (!(departdate1 >= today))
+    {
+      messages.push("Depart date must be today or greater than today");
     }
   }
+  if (roundtrip.checked == true) {
+    if (returnDate.value === "" || returnDate.value === null) {
+      messages.push("Please select your arrival travel date");
+    } else {
+      returnDate1 = new Date(returnDate.value);
+      if (!(returnDate1 >= today)) {
+        messages.push("Return date must be today or greater than today");
+      }
+      if (returnDate1 <= departDate1) {
+        messages.push("Return date must be greater than depart date");
+      }
+    }
+  } 
 }
 
 //Validation rule on credit card
 function validateCreditcard() {
   let regex = "^[0-9]*$";
-  //let today = new date();
-  
-  if (creditcard.value === '' || creditcard.value == null) {
-    messages.push('Credit card field is required')
+  if (creditcard.value === "" || creditcard.value == null) {
+    messages.push("Credit card field is required");
   } else if (!creditcard.value.match(regex)) {
-    messages.push('Credit card field can only be numbers')
+    messages.push("Credit card field can only be numbers");
   } else if (!(creditcard.value.length === 10)) {
-    messages.push('credit card field number has to be 10 values')
+    messages.push("credit card field number has to be 10 values");
+  }
+}
+
+//Should be similiar to Date validation
+function validateExpiryDate() {
+  let reg = /^(0[1-9]|1[0-2])\/(0[1-9]|1[1-9]|2[1-9])$/;
+  var date = new Date();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear().toString().slice(2);
+
+  var result = [];
+  if (expire.value === "" || expire.value === null) {
+    messages.push("Please enter expiry date of your card");
+  } else if (!reg.test(expire.value)) {
+    messages.push("Please enter in mm/yy format,only numbers allowed");
+  } else {
+    result = expire.value.toString().split("/");
   }
 
-  //Should be similiar to Date validation
-  /* if (expire.value < today) {
-    messages.push("Expiry date must be greater than today");
-  } */
+  var exMonth = result[0];
+  var exYear = result[1];
 
-  if (code.value === '' || code.value == null) {
-    messages.push('CSC field is required')
+  if (year > exYear || (year === exYear && month >= exMonth)) {
+    messages.push(
+      "The expiry date is before today's date. Please select a valid expiry date"
+    );
+  }
+}
+
+//validate From feild
+function validateFromField() {
+  if (from.value === "" || from.value == null) {
+    messages.push("Please select any city from From feild");
+  }
+}
+
+//validate To feild
+function validateToField() {
+  if (to.value === "" || to.value == null) {
+    messages.push("Please select any city from To feild");
+  }
+}
+
+//validate CSC value
+function validateCSC() {
+  let regex = "^[0-9]*$";
+  if (code.value === "" || code.value == null) {
+    messages.push("CSC field is required");
   } else if (!code.value.match(regex)) {
-    messages.push('CSC can only be numbers')
+    messages.push("CSC can only be numbers");
   } else if (!(code.value.length === 3)) {
-    messages.push('CSC number has to be 3 values')
+    messages.push("CSC number has to be 3 values");
   }
 }
 
 //Enable roundtrip date
 function enableRoundtrip() {
   returnDate.removeAttribute("disabled");
-  destinationCity.removeAttribute("disabled");
 }
 
 //Disable roundtrip date
 function disableRoundtrip() {
   returnDate.setAttribute("disabled", true);
-  destinationCity.setAttribute("disabled", true);
+}
+
+//set default data to all feilds on the form
+function setDefaultData() {
+  firstname.innerText = "";
+  lastname.innerText = "";
+  from.innerText = "";
+  to.innerText = "";
+  oneway.checked = "true";
+  roundtrip.checked = "false";
+  email.innerText = "";
+  phone.innerText = "";
+  departDate.innerText = new Date();
+  returnDate.innerText = new Date();
+  creditcard.innerText = "";
+  code.innerText = "";
+  expire.innerText = "";
+  departDate.innerText = "";
+  returnDate.innerText = "";
 }
